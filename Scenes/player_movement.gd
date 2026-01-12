21d3510f478d68e1e3003c2e89f0dfa4372058f3
Molflyn <jeremy-ly@hotmail.com>
2026-01-12 16:39:40 +0800

    Added player stun when colliding with NPC
    Player now gets stunned and pushed back when touching NPC

--- Scenes/player_movement.gd
+++ Scenes/player_movement.gd
@@ -1,23 +1,42 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var stun_timer = $StunTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _prsocess(delta: float) -> void:
-	pass
-
 @export var movement_speed : float = 500
 var character_direction : Vector2
 
-func _physics_process(delta):
+@export var stun_duration : int = 0.5
+@export var stun_velocity : float = 50.0
+var is_stunned : bool = false
+var stun_direction : Vector2
+
+func apply_knockback() -> void:
+	pass
+
+func stun() -> void:
+	if is_stunned:
+		return
+	
+	is_stunned = true
+	stun_timer.wait_time = stun_duration
+	stun_timer.start()
+	animated_sprite.play("stun")
+
+func _physics_process(delta: float) -> void:
+	if is_stunned:
+		velocity = stun_direction * stun_velocity
+		move_and_slide()
+		return
+	
 	character_direction.x = Input.get_axis("Left", "Right")
 	character_direction.y = Input.get_axis("Up", "Down")
 	character_direction = character_direction.normalized()
+	
 	if character_direction.x > 0: animated_sprite.flip_h = false
 	elif character_direction.x < 0: animated_sprite.flip_h = true
 	
@@ -30,3 +49,13 @@ func _physics_process(delta):
 		
 	move_and_slide()
 	
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("NPC"):
+		# Calculates reverse direction from collision
+		stun_direction = -(body.global_position - global_position).normalized()
+		stun()
+
+
+func _on_stun_timer_timeout() -> void:
+	is_stunned = false
